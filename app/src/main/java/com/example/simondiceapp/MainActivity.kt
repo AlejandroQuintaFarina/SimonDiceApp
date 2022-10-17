d2a8a2547c8f446fcc9320a1a5257ae648086e5b
AlejandroQuintaFarina <91197989+AlejandroQuintaFarina@users.noreply.github.com>
2022-10-17 10:53:21 +0200

    inicio del codigo + modificacion de la interfaz

--- app/src/main/java/com/example/simondiceapp/MainActivity.kt
+++ app/src/main/java/com/example/simondiceapp/MainActivity.kt
@@ -1,12 +1,51 @@
 package com.example.simondiceapp
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var secuencia = ArrayList<String>()
+    var ronda = 0
+    var numero = 4
+    var comprobaciones = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.layoutsimon)
+
+        binicio.setOnClickListener {
+            toast("Nueva partida")
+            ronda=1
+            numero=4
+            comprobaciones =4
+            nronda.setText(ronda.toString())
+            ncomprobaciones.setText("0")
+
+        }
+
+        bazul.setOnCLickListener {
+
+        }
+
+        bamarillo.setOnClickListener {
+
+        }
+
+        bverde.setOnClickListener {
+
+        }
+
+        brojo.setOnClickListener {
+
+        }
+
+
+
+
+
+
     }
+
+
 }
