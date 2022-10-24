09feb8a6d28ae2255a0466feae6a7e0e013b0ff4
AlejandroQuintaFarina <91197989+AlejandroQuintaFarina@users.noreply.github.com>
2022-10-24 10:44:47 +0200

    Creacion de diversos metodos (generarSecuencia y mostrar)

--- app/src/main/java/com/example/simondiceapp/MainActivity.kt
+++ app/src/main/java/com/example/simondiceapp/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.simondiceapp
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,7 +27,47 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-        val bamarillo : Button = findViewById(R.id.bamarillo)
+    fun Random.nextInt(range: IntRange): Int {
+        return range.start + nextInt(range.last - range.start)
+    }
+
+    fun generarSecuencia(){
+        val colores = ArrayList<String>()
+        colores.addAll(listOf("Rojo", "Azul", "Verde", "Amarillo"))
+        val random = Random()
+
+        println(random.nextInt(0..4))
+        secuencia.add(colores[random.nextInt(0..4)])
+        println(secuencia)
+        mostrar()
+    }
+
+    fun mostrar (){
+
+        for (i in 0..secuencia.size){
+            val muestra : String = secuencia[i]
+
+            if (muestra=="Rojo"){
+                start.text=""
+                start.setBackgroundColor(R.id.brojo)
+            }
+            else if (muestra=="Amarillo"){
+                start.text=""
+                start.setBackgroundColor(R.id.bamarillo)
+            }
+            else if (muestra=="Verde"){
+                start.text=""
+                start.setBackgroundColor(R.id.bverde)
+            }
+            else if (muestra=="Azul"){
+                start.text=""
+                start.setBackgroundColor(R.id.bazul)
+            }
+            else{
+                println("Esto no debería haber pasado")
+
+
+                val bamarillo : Button = findViewById(R.id.bamarillo)
         val bazul : Button = findViewById(R.id.bazul)
         val bverde : Button = findViewById(R.id.bverde)
         val brojo : Button = findViewById(R.id.brojo)
